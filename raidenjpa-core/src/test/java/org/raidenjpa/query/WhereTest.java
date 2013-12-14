c2f13a1413a1e1cc4377fe1c14627437853062f7
Andrei Tognolo <andrei@dextra-sw.com>
2013-12-14 10:37:12 -0200

    Struct to be able the switch between Hibernate and Raiden

--- raidenjpa-core/src/test/java/org/raidenjpa/query/WhereTest.java
+++ raidenjpa-core/src/test/java/org/raidenjpa/query/WhereTest.java
@@ -3,7 +3,6 @@ package org.raidenjpa.query;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
-import org.junit.Test;
 import org.raidenjpa.AbstractTestCase;
 import org.raidenjpa.util.QueryHelper;
 
@@ -12,18 +11,15 @@ public class WhereTest extends AbstractTestCase {
 	@Before
 	public void setUp() {
 		super.setUp();
-		
 		loadEntities();
 	}
 
-	@Test
 	public void testOneValue() {
 		QueryHelper query = new QueryHelper("SELECT a FROM A a WHERE a.stringValue = :a");
 		query.filter("a", "a");
 		assertEquals(1, query.getResultList().size());
 	}
 	
-	@Test
 	public void testOneAnd() {
 		QueryHelper query = new QueryHelper("SELECT a FROM A a WHERE a.stringValue = :stringValue AND a.intValue = :intValue");
 		query.filter("stringValue", "a");

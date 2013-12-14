e97685e235deec7dce07c01d2971b6c786330a54
Andrei Tognolo <andrei@dextra-sw.com>
2013-12-14 10:37:35 -0200

    Struct to be able the switch between Hibernate and Raiden

---
+++ raidenjpa-core/src/test/java/org/raidenjpa/query/HibernateWhereTest.java
@@ -0,0 +1,17 @@
+package org.raidenjpa.query;
+
+import org.junit.Test;
+
+public class HibernateWhereTest extends WhereTest {
+
+	@Test
+	public void testOneValue() {
+		super.testOneValue();
+	}
+	
+	@Test
+	public void testOneAnd() {
+		super.testOneAnd();
+	}
+
+}
---
+++ raidenjpa-core/src/test/java/org/raidenjpa/query/RaidenWhereTest.java
@@ -0,0 +1,16 @@
+package org.raidenjpa.query;
+
+import org.junit.Test;
+
+public class RaidenWhereTest extends WhereTest {
+
+	@Test
+	public void testOneValue() {
+		super.testOneValue();
+	}
+	
+	@Test
+	public void testOneAnd() {
+		super.testOneAnd();
+	}
+}

0460da108a90bd32317e10889f608d93a7a0a193
andreitognolo <andreitognolo@gmail.com>
2013-12-26 20:04:59 -0200

    Where

--- raidenjpa-core/src/test/java/org/raidenjpa/query/executor/WhereStackTest.java
+++ raidenjpa-core/src/test/java/org/raidenjpa/query/executor/WhereStackTest.java
@@ -11,44 +11,12 @@ import java.util.Map;
 import org.junit.Test;
 import org.raidenjpa.entities.A;
 import org.raidenjpa.query.parser.QueryParser;
-import org.raidenjpa.query.parser.WhereClause;
 import org.raidenjpa.query.parser.WhereElement;
-import org.raidenjpa.util.FixMe;
 
 public class WhereStackTest {
 
 	@Test
-	public void testPushOneExpression() {
-		WhereStack stackQuery = new WhereStack();
-		
-		String jpql = "SELECT a FROM A a WHERE a.stringValue = :stringValue"; 
-		WhereClause where = new QueryParser(jpql).getWhere();
-		
-		WhereElement firstExpression = where.nextElement();
-		assertEquals(RESOLVE, stackQuery.push(firstExpression));
-	}
-	
-	@Test
-	public void testPushTwoExpression() {
-		WhereStack whereStack = new WhereStack();
-		
-		String jpql = "SELECT a FROM A a WHERE a.stringValue = :stringValue AND a.intValue = :intValue";
-		
-		WhereClause where = new QueryParser(jpql).getWhere();
-		
-		WhereElement firstExpression = where.nextElement();
-		assertEquals(RESOLVE, whereStack.push(firstExpression));
-
-		WhereElement logicOperator = where.nextElement();
-		assertEquals(NOTHING, whereStack.push(logicOperator));
-		
-		WhereElement secondExpression = where.nextElement();
-		assertEquals(REDUCE, whereStack.push(secondExpression));
-	}
-	
-	@FixMe("After finished, analyse if this one make sense")
-	@Test
-	public void testResolve() {
+	public void testAndOperationReturningTrue() {
 		String jpql = "SELECT a FROM A a WHERE a.stringValue = :stringValue AND a.intValue = :intValue"; 
 		QueryParser queryParser = new QueryParser(jpql);
 		Map<String, Object> parameters = new HashMap<String, Object>();
@@ -69,5 +37,29 @@ public class WhereStackTest {
 		assertEquals(REDUCE, stackQuery.push(secondExpression));
 		stackQuery.reduce();
 		assertEquals(true, stackQuery.getResult());
+	}
+	
+	@Test
+	public void testAndOperationReturningFalse() {
+		String jpql = "SELECT a FROM A a WHERE a.stringValue = :stringValue AND a.intValue = :intValue"; 
+		QueryParser queryParser = new QueryParser(jpql);
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("stringValue", "a");
+		parameters.put("intValue", 2);
+		
+		WhereStack stackQuery = new WhereStack(new A("a", 1), queryParser, parameters);
+		
+		WhereElement firstExpression = queryParser.getWhere().nextElement();
+		assertEquals(RESOLVE, stackQuery.push(firstExpression));
+		stackQuery.resolve();
+		assertEquals(true, stackQuery.getResult());
+		
+		WhereElement logicOperator = queryParser.getWhere().nextElement();
+		assertEquals(NOTHING, stackQuery.push(logicOperator));
+		
+		WhereElement secondExpression = queryParser.getWhere().nextElement();
+		assertEquals(REDUCE, stackQuery.push(secondExpression));
+		stackQuery.reduce();
+		assertEquals(false, stackQuery.getResult());
 	}
 }

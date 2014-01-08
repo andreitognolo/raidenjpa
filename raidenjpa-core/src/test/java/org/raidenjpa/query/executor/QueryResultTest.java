606197989fbf02da3e6d9123bb469dc1fd0413c6
andreitognolo <java01@trn52.(none)>
2014-01-07 22:50:09 -0200

    More than one entity from

--- raidenjpa-core/src/test/java/org/raidenjpa/query/executor/QueryResultTest.java
+++ raidenjpa-core/src/test/java/org/raidenjpa/query/executor/QueryResultTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.junit.Test;
 import org.raidenjpa.entities.A;
+import org.raidenjpa.entities.B;
 
 public class QueryResultTest {
 
@@ -30,6 +31,24 @@ public class QueryResultTest {
 	
 	@Test
 	public void testAddFromTwice() {
+		QueryResult result = new QueryResult();
 		
+		List<A> as = new ArrayList<A>();
+		as.add(new A("a1"));
+		as.add(new A("a2"));
+		as.add(new A("a3"));
+		result.addFrom("a", as);
+		
+		List<B> bs = new ArrayList<B>();
+		bs.add(new B("b1"));
+		bs.add(new B("b2"));
+		result.addFrom("b", bs);
+		
+		Iterator<QueryResultRow> it = result.iterator();
+		A a1 = (A) it.next().get("a");
+		A a2 = (A) it.next().get("a");
+		
+		assertEquals("a1", a1.getStringValue());
+		assertEquals("a2", a2.getStringValue());
 	}
 }

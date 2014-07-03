5d1a963fed2c2698e3086b20afc33d73bf66ca35
pguatura <paula.guatura@dextra-sw.com>
2014-07-03 18:53:32 -0300

    criteria from test

--- raidenjpa-core/src/test/java/org/raidenjpa/query/criteria/CriteriaTest.java
+++ raidenjpa-core/src/test/java/org/raidenjpa/query/criteria/CriteriaTest.java
@@ -2,30 +2,21 @@ package org.raidenjpa.query.criteria;
 
 import static org.junit.Assert.assertEquals;
 
-import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.raidenjpa.AbstractTestCase;
 import org.raidenjpa.entities.A;
 import org.raidenjpa.entities.B;
+import org.raidenjpa.spec.criteria.RaidenCriteriaBuilder;
 import org.raidenjpa.spec.criteria.RaidenCriteriaQuery;
-import org.raidenjpa.util.EntityManagerUtil;
 
-public class CriteriaTest extends AbstractTestCase {
+public class CriteriaTest {
 
-	@Before
-	public void setUp() {
-		asRaiden();
-		truncate();
-	}
 
 	@Test
 	public void testSimpleFrom() {
-		EntityManager em = EntityManagerUtil.em();
-		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaBuilder builder = new RaidenCriteriaBuilder();
 		CriteriaQuery<A> criteria = builder.createQuery(A.class);
 		criteria.from(A.class);
 		assertEquals(((RaidenCriteriaQuery<A>) criteria).toJpql(), "FROM A a_0");
@@ -33,8 +24,7 @@ public class CriteriaTest extends AbstractTestCase {
 
 	@Test
 	public void testMultipleFrom() {
-		EntityManager em = EntityManagerUtil.em();
-		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaBuilder builder = new RaidenCriteriaBuilder();
 		CriteriaQuery<A> criteria = builder.createQuery(A.class);
 		criteria.from(A.class);
 		criteria.from(B.class);
